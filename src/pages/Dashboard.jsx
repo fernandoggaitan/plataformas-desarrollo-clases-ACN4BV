5d9afcd42a9cab5e528deb898989845d32230991
fernandoggaitan <fernandoggaitan@gmail.com>
2024-06-15 00:54:29 -0300

    Consumo del API desde el frontend

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,32 @@
+import axios from "axios"
+import { useAuth } from "../context/AuthContext"
+import { useState, useEffect } from "react";
+
 export const Dashboard = () => {
+
+    const {token} = useAuth();
+    const [welcome, setWelcome] = useState("");
+
+    useEffect(() => {
+        getWelcome();
+    }, []);
+
+    const getWelcome = async() => {
+        try{
+            const request = await axios.get("http://localhost:8888/welcome", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setWelcome(request.data.message);
+        }catch(error){
+            alert("Ha surgido un error, por favor intente más tarde");
+        }
+    }
 
     return (
         <>
-            <h1> Iniciaste sesión </h1>
+            <h1> {welcome} </h1>
         </>
     )
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,43 @@
+import { useState } from "react";
 import { useAuth } from "../context/AuthContext"
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Login = () => {
 
     const { login } = useAuth();
     const navigate = useNavigate();
 
-    const handlerLogin = () => {
-        login();
-        navigate("/dashboard");
+    const [email, setEmail] = useState("");
+    const [contrasena, setContrasena] = useState("");
+
+    const handlerLogin = async() => {
+        //login();
+        //navigate("/dashboard");
+        try{
+            const request = await axios.post("http://localhost:8888/login", {
+                email,
+                contrasena
+            });
+            if(request.data.success){
+                login(request.data.accessToken);
+                navigate("/dashboard");
+            }
+            alert(request.data.message);
+        }catch(error){
+            alert("Ha surgido un error, por favor intente más tarde");
+        }
     }
 
     return (
         <form onSubmit={(e) => e.preventDefault()}>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1"  />
+                <input type="email" className="form-control" id="exampleInputEmail1" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+                <input type="password" className="form-control" id="exampleInputPassword1" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
             </div>
             <button type="submit" className="btn btn-primary" onClick={handlerLogin}>Submit</button>
         </form>
